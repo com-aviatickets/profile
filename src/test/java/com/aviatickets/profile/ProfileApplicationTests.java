397a5e24a33a4699003909cda67a6928bf751ae0
Ruslan Pavlov <rpavlov@ubic.tech>
2025-01-07 18:27:15 +0600

    fix test

--- src/test/java/com/aviatickets/profile/ProfileApplicationTests.java
+++ src/test/java/com/aviatickets/profile/ProfileApplicationTests.java
@@ -3,7 +3,7 @@ package com.aviatickets.profile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.liquibase.enabled=false")
 class ProfileApplicationTests {
 
     @Test
